399ec832a91cdbefdbe535ca45d8ffab63e357b1
Niklas Anderson <nkanders@gmail.com>
2025-03-02 16:56:01 -0800

    Update problem 2 mem_controller Bring prob2 mem_controller in line with recent changes.

--- prob2/mem_controller.sv
+++ prob2/mem_controller.sv
@@ -19,16 +19,14 @@ module mem_controller #(
 
   // Store the address and next address in buffers
   logic [7:0] addr_buff, next_addr_buff;
-  // Should be set to 1 if this is a read, 0 if a write
-  logic is_read;
+  // Determine whether this is a read or write
+  // This is registered and updates with the state in order
+  // to avoid any race conditions that could result from it being
+  // a wire driven with continuous assignment
+  logic read, next_read;
   // Flag to indicate whether this page is one our memory
   // controller should respond to
   logic is_page;
-  // Flag to indicate whether to drive the bus or relinquish to CPU
-  logic drive_bus;
-  // Store data that comes from the CPU (data_write) and data to be
-  // read by the CPU (data_read)
-  logic [15:0] data_write, data_read;
   // Wire for memory connections
   wire [15:0] mem_data;
 
@@ -42,18 +40,10 @@ module mem_controller #(
   } state_t;
   state_t state, next_state;
 
-  // FIXME: Writing mostly works, except the last chunk of data is written
-  // correctly briefly, then overwritten with 'z
-  // Tri-state buffer control for AddrData and mem_data
-  assign drive_bus = is_read && state != WAIT_STATE;
-  assign bus.AddrData  = drive_bus ? data_read : 'z;
-  // mem_data (the mem module's Data inout) can be written to or allow the
-  // mem module to drive it
-  assign mem_data  = (!is_read && state != WAIT_STATE) ? data_write : 'z;
-  // If we're reading, allow the mem module to drive
-  assign data_read = is_read ? mem_data : 'z;
-  // If we're not reading, allow the CPU to drive data_write
-  assign data_write = !is_read ? bus.AddrData : 'z;
+  // If we're reading, allow the mem module to drive through mem_data
+  assign bus.AddrData = (read && state != WAIT_STATE) ? mem_data : 'z;
+  // If we're not reading, allow the CPU to drive mem_data
+  assign mem_data = (!read && state != WAIT_STATE) ? bus.AddrData : 'z;
 
   // Page mask
   localparam logic [15:0] PAGE_MASK = 16'hF000;
@@ -62,22 +52,11 @@ module mem_controller #(
   // Instantiate system memory
   mem system_mem (
       .clk (bus.clk),
-      .rdEn(is_read),
-      .wrEn(~is_read),
+      .rdEn(read),
+      .wrEn(~read),
       .Addr(addr_buff),
       .Data(mem_data)
   );
-
-  // FIXME: This lags behind one cycle, but is maybe less prone
-  // to the data races that could arise with continuous assign
-  // Capture data from the memory for reads
-  // otherwise write memory data to AddrData
-  // always_ff @(posedge clk) begin : read_data
-  //   if (state != WAIT_STATE) begin
-  //     if (is_read) data_read <= mem_data;
-  //     else data_write <= AddrData;
-  //   end
-  // end : read_data
 
   // State update transition
   always_ff @(posedge bus.clk or posedge bus.resetH) begin : update_state
@@ -87,17 +66,20 @@ module mem_controller #(
     else begin
       state <= next_state;
       addr_buff <= next_addr_buff;
+      read <= next_read;
     end
   end : update_state
 
   // Next state logic
   always_comb begin : next_state_logic
+    // Assume the same read status except for leaving the wait state
+    next_read = read;
     unique case (state)
       WAIT_STATE: begin
         if (bus.AddrValid && is_page) begin
           next_state = DATA1;
           next_addr_buff = bus.AddrData[7:0];
-          is_read = bus.rw;
+          next_read = bus.rw;
         end else begin
           next_state = WAIT_STATE;
         end
